de36f6967a5f001e3e8cfb135488beff2c5d832b
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2021-12-10 17:31:37 -0300

    refactor: Move o carregamento dos dados para uma função separada.

--- index.js
+++ index.js
@@ -12,43 +12,43 @@
         return {
 
             getData() {
-
-            },
-            setDataInit() {
-
                 const ajax = new XMLHttpRequest();
                 ajax.open('GET', './games.json');
                 ajax.send();
-
                 ajax.addEventListener('readystatechange', () => {
-                    let gameData = [];
                     if (ajax.readyState === 4 && ajax.status === 200) {
-
                         try {
                             gameData = JSON.parse(ajax.responseText);
-                            gameData = gameData.types
-
-                            $selectLotofacil.textContent = gameData[0].type;
-                            $selectLotofacil.style.color = gameData[0].color;
-                            $selectLotofacil.style.borderColor = gameData[0].color;
-
-                            $selectMegasena.textContent = gameData[1].type;
-                            $selectMegasena.style.color = gameData[1].color;
-                            $selectMegasena.style.borderColor = gameData[1].color;
-
-                            $selectLotomania.style.color = gameData[2].color;
-                            $selectLotomania.textContent = gameData[2].type;
-                            $selectLotomania.style.borderColor = gameData[2].color;
-
+                            this.setDataInit();
                         } catch (e) {
+                            console.log('ERRO');
                             console.log(e);
                         }
                     }
+                })
 
-                }, false);
+
+
+            },
+            setDataInit() {;
+                gameData = gameData.types;
+
+                $selectLotofacil.textContent = gameData[0].type;
+                $selectLotofacil.style.color = gameData[0].color;
+                $selectLotofacil.style.borderColor = gameData[0].color;
+
+                $selectMegasena.textContent = gameData[1].type;
+                $selectMegasena.style.color = gameData[1].color;
+                $selectMegasena.style.borderColor = gameData[1].color;
+
+                $selectLotomania.style.color = gameData[2].color;
+                $selectLotomania.textContent = gameData[2].type;
+                $selectLotomania.style.borderColor = gameData[2].color;
+
+                console.log(gameData);
             }
         }
     }
 
-    App().setDataInit();
+    App().getData();
 })();
