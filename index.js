910e4a435eb5df6ee3b866a5f0425a97b8af0569
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2021-12-10 18:12:52 -0300

    refactor: Adiciona eslint para formatar código

--- index.js
+++ index.js
@@ -1,54 +1,47 @@
 (() => {
-    'use strict';
+  const App = () => {
+    const $selectLotofacil = document.querySelector('[data-js="selectLotofacil"]');
+    const $selectMegasena = document.querySelector('[data-js="selectMegasena"]');
+    const $selectLotomania = document.querySelector('[data-js="selectLotomania"]');
 
-    const App = () => {
+    let gameData = [];
+    return {
 
-        const $selectLotofacil = document.querySelector('[data-js="selectLotofacil"]');
-        const $selectMegasena = document.querySelector('[data-js="selectMegasena"]');
-        const $selectLotomania = document.querySelector('[data-js="selectLotomania"]');
-
-
-        let gameData = [];
-        return {
-
-            getData() {
-                const ajax = new XMLHttpRequest();
-                ajax.open('GET', './games.json');
-                ajax.send();
-                ajax.addEventListener('readystatechange', () => {
-                    if (ajax.readyState === 4 && ajax.status === 200) {
-                        try {
-                            gameData = JSON.parse(ajax.responseText);
-                            this.setDataInit();
-                        } catch (e) {
-                            console.log('ERRO');
-                            console.log(e);
-                        }
-                    }
-                })
-
-
-
-            },
-            setDataInit() {;
-                gameData = gameData.types;
-
-                $selectLotofacil.textContent = gameData[0].type;
-                $selectLotofacil.style.color = gameData[0].color;
-                $selectLotofacil.style.borderColor = gameData[0].color;
-
-                $selectMegasena.textContent = gameData[1].type;
-                $selectMegasena.style.color = gameData[1].color;
-                $selectMegasena.style.borderColor = gameData[1].color;
-
-                $selectLotomania.style.color = gameData[2].color;
-                $selectLotomania.textContent = gameData[2].type;
-                $selectLotomania.style.borderColor = gameData[2].color;
-
-                console.log(gameData);
+      getData() {
+        const ajax = new XMLHttpRequest();
+        ajax.open('GET', './games.json');
+        ajax.send();
+        ajax.addEventListener('readystatechange', () => {
+          if (ajax.readyState === 4 && ajax.status === 200) {
+            try {
+              gameData = JSON.parse(ajax.responseText);
+              this.setDataInit();
+            } catch (e) {
+              console.log('ERRO');
+              console.log(e);
             }
-        }
-    }
+          }
+        });
+      },
+      setDataInit() {
+        gameData = gameData.types;
 
-    App().getData();
+        $selectLotofacil.textContent = gameData[0].type;
+        $selectLotofacil.style.color = gameData[0].color;
+        $selectLotofacil.style.borderColor = gameData[0].color;
+
+        $selectMegasena.textContent = gameData[1].type;
+        $selectMegasena.style.color = gameData[1].color;
+        $selectMegasena.style.borderColor = gameData[1].color;
+
+        $selectLotomania.style.color = gameData[2].color;
+        $selectLotomania.textContent = gameData[2].type;
+        $selectLotomania.style.borderColor = gameData[2].color;
+
+        console.log(gameData);
+      },
+    };
+  };
+
+  App().getData();
 })();
